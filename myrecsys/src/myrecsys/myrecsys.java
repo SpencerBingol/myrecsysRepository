4a0bcac6d016f65d75d947924cf7a689c3203c37
SpencerBingol <sbingol108@gmail.com>
2014-10-26 15:47:31 -0400

    Addition of Rating class.

--- myrecsys/src/myrecsys/myrecsys.java
+++ myrecsys/src/myrecsys/myrecsys.java
@@ -12,4 +12,54 @@ public class myrecsys {
 		System.out.printf("MYRESULTS Algorithm \t= %s\n", args[2]);
 		System.out.printf("MYRESULTS RMSE \t\t= %.6f\n", 0.0);
 	}
+	
+	static class Rating {
+		private int userID;
+		private int movieID;
+		private double rating;
+		private double predicted;
+		
+		public Rating(int userID, int movieID, double rating) {
+			this.userID = userID;
+			this.movieID = movieID;
+			this.rating = rating;
+			this.predicted = -1;
+			
+			if (this.rating > 5 || this.rating < 1) this.rating=-1;
+		}
+		
+		public Rating(Rating other) {
+			userID = other.getUser();
+			movieID = other.getMovie();
+			rating = other.getRating();
+		}
+		
+		public void setPredicted(double score) {
+			this.predicted = score;
+		}
+		
+		public double getPredicted() {
+			return predicted;
+		}
+		
+		public int getUser() {
+			return userID;
+		}
+		
+		public int getMovie() {
+			return movieID;
+		}
+		
+		public double getRating() {
+			return rating;
+		}
+		
+		public String toString() {
+			return "User: " + userID + " Movie: " + movieID + " Rating: " + rating;
+		}
+		
+		public boolean isValid() {
+			return !(userID == -1 || movieID == -1 || rating == -1);
+		}
+	}
 }
