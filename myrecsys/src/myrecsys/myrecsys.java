3229aaf5a95ce02573bcf299ca585d3da19655e6
SpencerBingol <sbingol108@gmail.com>
2014-10-26 15:55:36 -0400

    Added rmse() and parse files functionality.

--- myrecsys/src/myrecsys/myrecsys.java
+++ myrecsys/src/myrecsys/myrecsys.java
@@ -31,6 +31,95 @@ public class myrecsys {
 		}
 	}
 	
+	static void parseRatingsFiles(String groupBy, String trainFile, String testFile) {
+		BufferedReader br; 										// buffered reader for each file.
+		String line, tmp[];										// line - each line of the files. tmp - each line split by space " ".
+		ArrayList<Rating> cur;									// the current Rating arraylist being worked with.
+		
+		try {
+			br = new BufferedReader(new FileReader(trainFile));	// training file buffered reader.
+			while ( (line=br.readLine()) != null) {				// while there are still more lines.
+				tmp = line.split("\t");							
+				
+				if (tmp.length!=4) {							// expecting lines in the format "user | movie | rating | timestamp"
+					System.out.printf("File formatting error for training data file \'%s\'.\n", trainFile);
+					System.exit(3);
+				} 
+				
+				Rating r = new Rating(parseIntCheck(tmp[0]), parseIntCheck(tmp[1]), parseIntCheck(tmp[2]));	// create a new rating for this review
+				if(!r.isValid()) {													// ensures the Rating was created correctly.
+					System.out.println("There's been a rating formatting error.");
+					System.exit(5);
+				}
+		
+				if (!ratingsByUser.containsKey(r.getUser())) ratingsByUser.put(r.getUser(), new ArrayList<Rating>());
+				cur = ratingsByUser.get(r.getUser());
+				cur.add(r);
+				ratingsByUser.put(r.getUser(), cur);
+			
+				if (!ratingsByMovie.containsKey(r.getMovie())) ratingsByMovie.put(r.getMovie(), new ArrayList<Rating>());
+				cur = ratingsByMovie.get(r.getMovie());
+				cur.add(r);
+				ratingsByMovie.put(r.getMovie(), cur);
+			} br.close();
+		} catch (java.io.IOException e) {
+			System.out.printf("Unable to open training data file \'%s\'.\n", trainFile);
+			System.exit(2);
+		} 
+		
+		try {															// repeat this whole process for the test data file.
+			br = new BufferedReader(new FileReader(testFile));
+			while ( (line=br.readLine())!= null) {
+				tmp = line.split("\t");
+				
+				if (tmp.length!=4) {
+					System.out.printf("File formatting error for test data file \'%s\'.\n", testFile);
+					System.exit(4);
+				} 
+				
+				Rating r = new Rating(parseIntCheck(tmp[0]), parseIntCheck(tmp[1]), parseIntCheck(tmp[2]));
+				if(!r.isValid()) {
+					System.out.println("There's been a rating formatting error.");
+					System.exit(5);
+				}
+		
+				if (groupBy.equals("user")) {
+					if (!testRatings.containsKey(r.getUser())) testRatings.put(r.getUser(), new ArrayList<Rating>());
+					cur = testRatings.get(r.getUser());
+					cur.add(r);
+					testRatings.put(r.getUser(), cur);
+				}
+				else if (groupBy.equals("movie")) {
+					if (!testRatings.containsKey(r.getMovie())) testRatings.put(r.getMovie(), new ArrayList<Rating>());
+					cur = testRatings.get(r.getMovie());
+					cur.add(r);
+					testRatings.put(r.getMovie(), cur);
+				}
+			} br.close();
+		} catch (java.io.IOException e) {
+			System.out.printf("Unable to open test data file \'%s\'.\n", testFile);
+			System.exit(3);
+		}
+	}
+	
+	
+	static double rmse() {											
+		double sum = 0;						// sum - sum of differences between predicted ratings and actual ratings, squared.
+		int count = 0;						// count - number of ratings in the test data.
+		for (int i : testRatings.keySet()) {
+			ArrayList<Rating> tmp = testRatings.get(i);
+			
+			for (Rating r : tmp) {
+				if (r.getPredicted()!=-1) {
+					count++;
+					sum += Math.pow(r.getPredicted() - r.getRating(), 2);
+				} 
+			}
+		} 
+		
+		return Math.sqrt(sum/count);
+	}
+	
 	static class Rating {
 		private int userID;
 		private int movieID;
